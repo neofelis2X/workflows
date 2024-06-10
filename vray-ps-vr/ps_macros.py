83937e62eb44029b96a89d803f9dbac3e410e009
neofelis2X <mkneidinger@gmx.net>
2024-06-10 17:14:23 +0200

    first draft of photoshop macros

--- vray-ps-vr/ps_macros.py
+++ vray-ps-vr/ps_macros.py
@@ -1,11 +1,81 @@
 #!/usr/bin/env/ python3
 
+# pip install pypiwin32
+#from comtypes.client import GetActiveObject
+from win32com.client import Dispatch, GetActiveObject
+import logging
+
+PS_MM = 4
+PS_PIXEL = 1
+PS_NORMAL_LAYER = 1
+PS_SMART_OBJECT_LAYER = 17
+PS_DISPLAY_NO_DIALOGS = 3
+
 def update_all_smartlayer(file, images, log):
     raise NotImplementedError
 
 def create_new_psd(image_layers, log):
-    raise NotImplementedError
+    app = _prepare_photoshop(log)
+    if not app:
+        return
+
+    start_ruler_units = app.Preferences.RulerUnits
+    app.Preferences.RulerUnits = PS_PIXEL
+
+    filename = "my test doc"
+    docRef = app.Documents.Add(40, 40, 72.0, filename)
+    log.debug("Created new document in photoshop with the name: '%s'" % filename)
+
+    new_layer = _create_new_smart_layer(app, docRef, 'smartlayer', log)
+
+    #new_layer.blendMode = 3
+
+    new_path = "c:\\Users\\Matthias Kneidinger\\Downloads\\4795-specstrength.jpg"
+    _replace_image_smart_layer(app, new_layer, new_path)
+
+    # Make sure to set the ruler units prior to creating the document.
+    app.Preferences.RulerUnits = start_ruler_units
+
+
+def _prepare_photoshop(log):
+    try:
+        app = GetActiveObject("Photoshop.Application")
+
+    except:
+        log.warning("Couldn't access Photoshop. Please make sure the application is running!")
+        return False
+
+    log.debug("Successfully attached photoshop.")
+
+
+    app.displayDialogs = PS_DISPLAY_NO_DIALOGS
+
+    return app
+
+def _create_new_smart_layer(app, doc, name:str, log):
+    layerRef = doc.ArtLayers.Add()
+    layerRef.name = name
+    layerRef.Kind = PS_NORMAL_LAYER
+
+    desc = Dispatch('Photoshop.ActionDescriptor')  # empty descriptor
+    app.ExecuteAction(app.StringIDToTypeID("newPlacedLayer"), desc, PS_DISPLAY_NO_DIALOGS)
+
+    return layerRef
+
+def _replace_image_smart_layer(app, layer, new_img_path: str):
+    id_replace = app.StringIDToTypeID('placedLayerReplaceContents');
+    desc = Dispatch('Photoshop.ActionDescriptor')  # empty descriptor
+    desc.PutPath(app.CharIDToTypeID('null'), new_img_path)
+    app.ExecuteAction(id_replace, desc, PS_DISPLAY_NO_DIALOGS);
+
 
 if __name__ == "__main__":
-    pass
+
+    layers = []
+
+    logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger('ps_macros')
+    logger.setLevel(logging.DEBUG)
+
+    create_new_psd(layers, logger)
 
---
+++ vray-ps-vr/vray_script_setup.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env/ python3
+
+import rhinoscriptsyntax as rs
+import rh8VRay as vray
+import os.path
+import time
+from datetime import datetime
+
+print("VRay Version: ", vray.Version, "Core: ", vray.VRayVersion)
+
+VRTOUR_RENDERSETTINGS = ""
+OUTPUT_PATH = os.path.normpath("C:\\Users\\Matthias Kneidinger\\Downloads\\photoshop_python\\test\\")
+
+# https://docs.chaos.com/display/VRHINO/V-Ray+Script+Access
+# To use the V-Ray for Rhino wrapper module, add a new Python
+# Module Search path (Tools/Options/Python 3) pointing to:
+# C:\Program Files\Chaos Group\V-Ray\V-Ray for Rhinoceros
+
+# RenderModes
+# RM_PRODUCTION		0	Renders in production mode
+# RM_INTERACTIVE	1	Renders in interactive mode
+# RM_CLOUD			2	Starts the current render job on the Cloud
+# RM_LAST			3	Repeats the last render
+
+# RenderEngines
+# RE_CPU	0	Renders on the CPU
+# RE_CUDA	1	Renders on the GPU and/or CPU using CUDA
+# RE_RTX	2	Renders on the GPU using RTX Optix
+
+def render_view():
+	t_start = datetime.now()
+	print(f"Start render at: {t_start}")
+
+	vray.Render(0, 1, -1)
+
+	t_end = datetime.now()
+	print(f"Finished render at: {t_end}")
+	diff = t_end - t_start
+	print(f"Render took: {round(diff.total_seconds(), 2)} s")
+
+	vray.RefreshUI()
+
+def change_save_path(filename: str, fileending: str):
+	path = os.path.join(OUTPUT_PATH, filename + fileending)
+	print(path)
+
+	with vray.Scene.Transaction() as t:
+		vray.Scene.SettingsOutput.save_render_path = path
+		vray.Scene.SettingsOutput.img_file = path
+		vray.Scene.SettingsOutput.img_dir = path
+
+cudaDevices = vray.GetDeviceList(vray.RenderEngines.RE_CUDA)
+
+for device in cudaDevices:
+	print(device.Name)
+	if 'NVIDIA' in device.Name:
+		use = device.UseForRendering
+
+# success = vray.Scene.LoadSettings(VRTOUR_RENDERSETTINGS)
+
+params = vray.Scene.SettingsOutput.ParamNames
+for par in params:
+	pass
+	#print(par)
+
+with vray.Scene.Transaction() as t:
+	vray.Scene.SettingsOutput.img_width = 800
+	vray.Scene.SettingsOutput.img_height = 400
+	vray.Scene.SettingsOutput.save_render = 1
+	vray.Scene.SettingsOutput.img_noAlpha = 0
+
+success = vray.SetDeviceList(vray.RenderEngines.RE_CUDA, [0, 1])
+
+views = rs.NamedViews()
+if views:
+	for view in views:
+		print(f"Current view: {view}")
+		change_save_path(view, '.png')
+		rs.RestoreNamedView(view)
+		rs.Redraw()
+		render_view()
+
